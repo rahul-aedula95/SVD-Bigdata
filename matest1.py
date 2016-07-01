7c793ba9cee4330c828c8340965d98a909ae6844
Rahul Aedula <rahulaedula95@gmail.com>
2016-07-01 22:01:58 +0530

    Add files via upload

--- matest1.py
+++ matest1.py
@@ -5,21 +5,21 @@ for line in sys.stdin:
     data = line.strip().split("\t")
     
 
-b = np.zeros(shape=(4,4))
+b = np.zeros(shape=(1000,1000))
 #b = np.loadtxt(sys.stdin)
-c = np.zeros(shape=(4,5))
+c = np.zeros(shape=(1000,1001))
 key = 1
-for i in range(0,4):
+for i in range(0,1000):
     c[i][0] = key
     key = key+1
-for k in range(0,4):
-    for l in range(0,4):
+for k in range(0,1000):
+    for l in range(0,1000):
          c[k][l+1] = b[k][l]
 
 
 
   
-for m in range(0,4):
-    for n in range(0,5):
+for m in range(0,1000):
+    for n in range(0,1001):
         print "%d \t"%c[m][n],#sys.stdout.write("%.2f\t"%(np.array(c[m][n])))
     print 
